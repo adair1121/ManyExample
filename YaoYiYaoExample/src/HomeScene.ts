1d8fb33968f2e331d4d4bcbd190b71fc8aaad0fd
chenkai <chenkai@bluemoon.com.cn>
2017-04-20 14:48:57 +0800

    +增加自动滚动聊天文本Demo

--- YaoYiYaoExample/src/HomeScene.ts
+++ YaoYiYaoExample/src/HomeScene.ts
@@ -16,24 +16,21 @@ class HomeScene extends eui.Component{
 	}
 
 	public childrenCreated(){
+		this.shakeTest();
+	}
+
+	private shakeTest(){
 		this.shakeTool = new ShakeTool();
 		this.shakeTool.addEventListener(egret.Event.CHANGE, this.onChange,this);
-		this.shakeTool.start();	
+		this.shakeTool.start();
 	}
 
 	private onChange(e:egret.Event){
 		var data = e.data;
-
-		//显示测试数据
-		this.label0.text = "x:" + data.x;
-		this.label1.text = "y:" + data.y;
-		this.label2.text = "z:" + data.z;
-		this.label3.text = "shakeCount:" + data.shakeCount;
-
 		//用户大概晃动了手机2-3次
 		if(data.shakeCount > 6){
-			//egret.log("摇一摇完成");
-			//this.shakeTool.stop();
+			egret.log("摇一摇完成");
+			this.shakeTool.stop();
 		}
 	}
 }
